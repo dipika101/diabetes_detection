c8ea84003fcfd0f5426c33440f1c8b2f67883c24
dipika <63471537+dipika101@users.noreply.github.com>
2021-10-23 05:36:02 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-%%writefile app.py
  
 import pickle
 import streamlit as st
@@ -13,7 +12,23 @@ classifier = pickle.load(pickle_in)
 def prediction(Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age):   
  
     # Pre-processing user input    
-
+#     if Gender == "Male":
+#         Gender = 0
+#     else:
+#         Gender = 1
+ 
+#     if Married == "Unmarried":
+#         Married = 0
+#     else:
+#         Married = 1
+ 
+#     if Credit_History == "Unclear Debts":
+#         Credit_History = 0
+#     else:
+#         Credit_History = 1  
+ 
+#     LoanAmount = LoanAmount / 1000
+ 
     # Making predictions 
     prediction = classifier.predict( 
         [[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
